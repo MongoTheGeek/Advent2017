36a1b2b47bc5ea0aa9d5168d8cc9b7532bce5cc8
Mark Lively <lively@accuweather.com>
2017-12-25 10:41:43 -0500

    All done!

--- 25/main.swift
+++ 25/main.swift
@@ -8,5 +8,88 @@
 
 import Foundation
 
-print("Hello, World!")
 
+enum State{
+    case a,b,c,d,e,f
+}
+
+var tape = Dictionary<Int, Bool>()
+var position = 0
+var state = State.a
+
+for _ in 1...12459852{
+    var value = false
+    if tape.keys.contains(position){
+        value = tape[position]!
+    } else {
+        tape[position] = value
+    }
+    switch state {
+    case .a:
+        if value {
+            tape[position]=true
+            position -= 1
+            state = .e
+        } else {
+            tape[position]=true
+            position += 1
+            state = .b
+        }
+    case .b:
+        if value {
+            tape[position]=true
+            position += 1
+            state = .f
+        } else {
+            tape[position]=true
+            position += 1
+            state = .c
+        }
+    case .c:
+        if value {
+            tape[position]=false
+            position += 1
+            state = .b
+        } else {
+            tape[position]=true
+            position -= 1
+            state = .d
+        }
+    case .d:
+        if value {
+            tape[position]=false
+            position -= 1
+            state = .c
+        } else {
+            tape[position]=true
+            position += 1
+            state = .e
+        }
+    case .e:
+        if value {
+            tape[position]=false
+            position += 1
+            state = .d
+        } else {
+            tape[position]=true
+            position -= 1
+            state = .a
+        }
+    case .f:
+        if value {
+            tape[position]=true
+            position += 1
+            state = .c
+        } else {
+            tape[position]=true
+            position += 1
+            state = .a
+        }
+    }
+}
+
+var total = 0
+for  k in tape.keys{
+    if tape[k]! {total += 1}
+}
+print(total)
